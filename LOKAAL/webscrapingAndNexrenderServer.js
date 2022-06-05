3d889c028700f79af0d367d7881c0d02c368a6e9
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2022-06-05 16:12:16 +0200

    add end composition nexrender

--- LOKAAL/webscrapingAndNexrenderServer.js
+++ LOKAAL/webscrapingAndNexrenderServer.js
@@ -96,10 +96,11 @@ app.post("/render", (req, res) => {
     spotify: req.body.spotify,
   };
   res.sendStatus(200);
-  main(res, data).catch(console.error);
+  all(res, data).catch(console.error);
+  end(res, data).catch(console.error);
 });
 
- main = async (res, data) => {
+const all = async (res, data) => {
   const result = await client.addJob({
     template: {
       src: `file:///Users/joppe.rabijns/Desktop/V1/1080p_Tracking.aep`,
@@ -165,48 +166,116 @@ app.post("/render", (req, res) => {
           output: "encoded.mp4",
         },
         {
-          "module": "@nexrender/action-upload",
+          module: "@nexrender/action-upload",
           input: "encoded.mp4",
-          "provider": "ftp",
-          "params": {
-              "host": "ftp.rabijnsbe.webhosting.be",
-              "port": 21,
-              "user": "joppe@rabijnsbe",
-              "password": "VdAkfDA5JvcnFX!",
-              "output": `/media/${data.facebook.id}.mp4`
-          }
-      }
+          provider: "ftp",
+          params: {
+            host: "ftp.rabijnsbe.webhosting.be",
+            port: 21,
+            user: "joppe@rabijnsbe",
+            password: "VdAkfDA5JvcnFX!",
+            output: `/media/${data.facebook.id}.mp4`,
+          },
+        },
       ],
     },
   });
   result.on("created", (job) => console.log("project has been created"));
   result.on("started", (job) => console.log("start"));
   result.on("progress", (job, percents) => console.log(percents));
-  result.on("finished", (job) => /* res.sendStatus(200) */ console.log("finsihed"));
+  result.on("finished", (job) =>
+    /* res.sendStatus(200) */ console.log("finsihed")
+  );
   /*   concat(data.facebook.id, res)); */
   result.on("error", (err) => console.log("project rendering error", err));
 };
 
-/* function concat(facebookId, res) {
-  ffmpeg("/Users/joppe.rabijns/Desktop/Finalwork_Nexrender_Outputs/MAN.mp4")
-    .input(
-      `/Users/joppe.rabijns/Desktop/Finalwork_Nexrender_Outputs/${facebookId}.mp4`
-    )
-    .input(`/Users/joppe.rabijns/Desktop/Finalwork_Nexrender_Outputs/TEKST_2.mp4`)
-    .on('progress', function(progress) {
-      console.log('Processing: ' + progress.percent + '% done');
-    })
-    .on("error", function (err) {
-      console.log("An error occurred: " + err.message);
-    })
-    .on("end", function () {
-      console.log("Merging finished !");
-      res.sendStatus(200);
-    })
-    .mergeToFile(
-      `/Users/joppe.rabijns/Desktop/Finalwork_Nexrender_Outputs/${facebookId}-1.mp4`
-    );
-} */
+const end = async (res, data) => {
+  const result = await client.addJob({
+    template: {
+      src: `file:///Users/joppe.rabijns/Desktop/V1/1080p_Tracking.aep`,
+      composition: `END`,
+      outputExt: "mov",
+      outputModule: "FINALWORK",
+    },
+    assets: [
+      {
+        type: "data",
+        layerName: "NAAM",
+        property: "Source Text",
+        value: `${data.facebook.name}`,
+        composition: `END->PANCARTE`,
+      },
+      {
+        type: "data",
+        layerName: "WOONPLAATS",
+        property: "Source Text",
+        value: `${data.fingerprint.ipLocation.city.name}`,
+        composition: `END->PANCARTE`,
+      },
+      {
+        type: "data",
+        layerName: "DATUM",
+        property: "Source Text",
+        value: `${data.facebook.birthday}`,
+        composition: `END->PANCARTE`,
+      },
+      {
+        type: "data",
+        layerName: "GSM",
+        property: "Source Text",
+        value: `${data.fingerprint.ip}`,
+        composition: "END->PANCARTE",
+      },
+      {
+        type: "data",
+        layerName: "EMAIL",
+        property: "Source Text",
+        value: `${data.facebook.email}`,
+        composition: "END->PANCARTE",
+      },
+      {
+        type: "data",
+        layerName: "MOMENTEEL",
+        property: "Source Text",
+        value: `${data.fingerprint.ipLocation.city.name}`,
+        composition: "END->PANCARTE",
+      },
+      {
+        type: "image",
+        layerName: "FOTO",
+        src: `${data.facebook.picture.data.url}`,
+        composition: "END->PANCARTE->FOTO",
+      },
+    ],
+    actions: {
+      postrender: [
+        {
+          module: "@nexrender/action-encode",
+          preset: "mp4",
+          output: "encoded.mp4",
+        },
+        {
+          module: "@nexrender/action-upload",
+          input: "encoded.mp4",
+          provider: "ftp",
+          params: {
+            host: "ftp.rabijnsbe.webhosting.be",
+            port: 21,
+            user: "joppe@rabijnsbe",
+            password: "VdAkfDA5JvcnFX!",
+            output: `/media/${data.facebook.id}-2.mp4`,
+          },
+        },
+      ],
+    },
+  });
+  result.on("created", (job) => console.log("project has been created"));
+  result.on("started", (job) => console.log("start"));
+  result.on("progress", (job, percents) => console.log(percents));
+  result.on("finished", (job) => console.log("finsihed"));
+  result.on("error", (err) => console.log("project rendering error", err));
+};
 
 const listener = app.listen(4000, () => {
   console.log("App is listening on port " + listener.address().port);
