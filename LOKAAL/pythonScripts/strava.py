15876420364512f8be2bb907a2f0a17bc47e6445
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2022-06-07 18:35:07 +0200

    strava python

--- LOKAAL/pythonScripts/strava.py
+++ LOKAAL/pythonScripts/strava.py
@@ -1,15 +1,43 @@
-from selenium import webdriver
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 import json
-from selenium.webdriver.chrome.options import Options
-
-################
-## Parameters ##
-################
 import sys
 
 searchname = sys.argv[1]
+
+def selenium(driver, searchname):
+  driver.get("https://www.strava.com/login")
+  time.sleep(2)
+  username = driver.find_element_by_id("email")
+  username.send_keys("joppe.rabijns@gmail.com")  
+  pword = driver.find_element_by_id("password")
+  pword.send_keys("E-vBi*St,-S2?dE")        
+  driver.find_element_by_id("login-button").click()
+  time.sleep(2)
+  driver.find_element_by_id("open-global-search-button").click()
+  time.sleep(1)
+  search = driver.find_element_by_id("global-search-field")
+  search.send_keys(searchname)  
+  time.sleep(1)
+  driver.find_element_by_id("global-search-button").click()
+  time.sleep(2)
+  driver.find_element_by_class_name("text-headline").click()
+  time.sleep(2)
+  driver.find_element_by_class_name("athlete-search").find_element_by_tag_name('a').click()
+  time.sleep(2)
+
+def bs4(driver):
+  src = driver.page_source
+  soup = BeautifulSoup(src, 'html.parser')
+  athlete_pictures = soup.find('ul', {'class': 'MediaThumbnailList--list--boXGW'})
+  athlete_first_picture = athlete_pictures.find("img")
+  strava_data = {
+    "latest_image": athlete_first_picture["src"],
+  }
+  print(json.dumps(strava_data))
 
 while True:
   try:
@@ -17,39 +45,9 @@ while True:
     options.binary_location = '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser'
     driver_path = '/Applications/chromedriver'
     driver = webdriver.Chrome(options = options, executable_path = driver_path)
-    driver.get("https://www.strava.com/login")
-    time.sleep(2)
-    username = driver.find_element_by_id("email")
-    username.send_keys("joppe.rabijns@gmail.com")  
-    pword = driver.find_element_by_id("password")
-    pword.send_keys("E-vBi*St,-S2?dE")        
-    driver.find_element_by_id("login-button").click()
-    time.sleep(2)
-    driver.find_element_by_id("open-global-search-button").click()
-    time.sleep(1)
-    search = driver.find_element_by_id("global-search-field")
-    search.send_keys(searchname)  
-    time.sleep(1)
-    driver.find_element_by_id("global-search-button").click()
-    time.sleep(2)
-    driver.find_element_by_class_name("text-headline").click()
-    time.sleep(2)
-    driver.find_element_by_class_name("athlete-search").find_element_by_tag_name('a').click()
-    time.sleep(2)
-    src = driver.page_source
-    soup = BeautifulSoup(src, 'html.parser')
-    athleteProfile = soup.find('div', {'id': 'athlete-profile'})
-    name_loc = athleteProfile.find("h1")
-    name = name_loc.get_text().strip()
-    location_loc = athleteProfile.find("div", class_="location")
-    location = location_loc.get_text().strip()
-    strava_data = {
-      "name": name,
-      "location": location,
-    }
-    print(json.dumps(strava_data))
+    selenium(driver, searchname)
+    bs4(driver)
     break
   except:
     print("error")
     break
-driver.quit()
