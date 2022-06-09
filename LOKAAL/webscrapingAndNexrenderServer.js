3058104cbdeb9df394bf537068c8e0e5f4781abd
Joppe Rabijns <joppe.rabijns@student.ehb.be>
2022-06-09 15:35:18 +0200

    webscraping rewrite

--- LOKAAL/webscrapingAndNexrenderServer.js
+++ LOKAAL/webscrapingAndNexrenderServer.js
@@ -55,69 +55,48 @@ app.post("/spotify", (req, res) => {
   });
 });
 
-//https://stackoverflow.com/questions/66257999/res-many-videos-nodejs
-app.get("/getVideo", (req, res) => {
-  const path = `/Users/joppe.rabijns/Desktop/Finalwork_Nexrender_Outputs/${req.query.facebookID}.mp4`;
-  const stat = fs.statSync(path);
-  const fileSize = stat.size;
-  const range = req.headers.range;
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-");
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-    const chunksize = end - start + 1;
-    const file = fs.createReadStream(path, { start, end });
-    const head = {
-      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": chunksize,
-      "Content-Type": "video/mp4",
-    };
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      "Content-Length": fileSize,
-      "Content-Type": "video/mp4",
-    };
-    res.writeHead(200, head);
-    fs.createReadStream(path).pipe(res);
-  }
-});
-
 app.post("/render", (req, res) => {
-  let data = {
-    facebook: req.body.facebook,
-    fingerprint: req.body.fingerprint,
-    linkedIn: req.body.linkedIn,
-    instagram: req.body.instagram,
-    strava: req.body.strava,
-    spotify: req.body.spotify,
-    phonenumber: req.body.phonenumber,
-  };
   res.sendStatus(200);
-  all(res, data).catch(console.error);
-  end(res, data).catch(console.error);
+
+  let nexrenderData = () => {
+    let gender;
+
+    let initialDate = req.body.facebook.birthday.split(/\//);
+    let date = [initialDate[1], initialDate[0], initialDate[2]].join("/");
+
+    if (req.body.facebook.gender === "male") {
+      gender = "Man";
+    } else if (req.body.facebook.gender === "female") {
+      gender = "Vrouw";
+    }
+    return {
+      phonenumber: req.body.phonenumber,
+      facebook_name: req.body.facebook.name,
+      facebook_gender: gender,
+      facebook_date: date,
+      facebook_hometown: hometown,
+      facebook_email: req.body.facebook.email,
+      fingerprint_momenteel: req.body.fingerprint.ipLocation.city.name,
+      linkedin_education: req.body.linkedIn.education,
+      linkedin_experience: req.body.linkedIn.experience,
+      facebook_foto: req.body.facebook.picture.data.url,
+      instagram_followers: req.body.instagram.followers_count,
+      instagram_images: req.body.instagram.images,
+      strava_image: req.body.strava.latest_image,
+      strava_club: req.body.strava.club,
+      strava_follower: req.body.strava.follower,
+      spotify_follower: req.body.spotify.follower,
+      spotify_playlist: req.body.spotify.playlist,
+    };
+  };
+
+  console.log(nexrenderData);
+
+  all(res, nexrenderData).catch(console.error);
+  end(res, nexrenderData).catch(console.error);
 });
 
 const all = async (res, data) => {
-  let gender;
-  let hometown;
-  let initialDate = data.facebook.birthday.split(/\//);
-  let date = [initialDate[1], initialDate[0], initialDate[2]].join("/");
-
-  if (data.facebook.gender === "male") {
-    gender = "Man";
-  } else if (data.facebook.gender === "female") {
-    gender = "Vrouw";
-  }
-
-  if (data.facebook.hometown.name) {
-    hometown = data.facebook.hometown.name.split(",")[0];
-  } else {
-    hometown = "undefined";
-  }
-
   const result = await client.addJob({
     template: {
       src: `file:///Users/joppe.rabijns/Desktop/NEXRENDER_V1/1080p_Tracking.aep`,
@@ -130,28 +109,21 @@ const all = async (res, data) => {
         type: "data",
         layerName: "NAAM",
         property: "Source Text",
-        value: `${data.facebook.name}`,
-        composition: `ALL->PANCARTE`,
-      },
-      {
-        type: "data",
-        layerName: "WOONPLAATS",
-        property: "Source Text",
-        value: `${hometown}`,
+        value: `${data.facebook_name}`,
         composition: `ALL->PANCARTE`,
       },
       {
         type: "data",
         layerName: "DATUM",
         property: "Source Text",
-        value: `${date}`,
+        value: `${facebook_date}`,
         composition: `ALL->PANCARTE`,
       },
       {
         type: "data",
         layerName: "GESLACHT",
         property: "Source Text",
-        value: `${gender}`,
+        value: `${data.facebook_gender}`,
         composition: `ALL->PANCARTE`,
       },
       {
@@ -165,34 +137,34 @@ const all = async (res, data) => {
         type: "data",
         layerName: "EMAIL",
         property: "Source Text",
-        value: `${data.facebook.email}`,
+        value: `${data.facebook_email}`,
         composition: "ALL->PANCARTE",
       },
       {
         type: "data",
         layerName: "MOMENTEEL",
         property: "Source Text",
-        value: `Omgeving van ${data.fingerprint.ipLocation.city.name}`,
+        value: `Omgeving van ${data.fingerprint_momenteel}`,
         composition: "ALL->PANCARTE",
       },
       {
         type: "data",
         layerName: "STUDIE",
         property: "Source Text",
-        value: `${data.linkedIn.education}`,
+        value: `${data.linkedin_education}`,
         composition: "ALL->PANCARTE",
       },
       {
         type: "data",
         layerName: "WERK",
         property: "Source Text",
-        value: `${data.linkedIn.experience}`,
+        value: `${data.linkedin_experience}`,
         composition: "ALL->PANCARTE",
       },
       {
         type: "image",
         layerName: "FOTO",
-        src: `${data.facebook.picture.data.url}`,
+        src: `${data.facebook_foto}`,
         composition: "ALL->PANCARTE->FOTO",
       },
     ],
@@ -229,24 +201,6 @@ const all = async (res, data) => {
 };
 
 const end = async (res, data) => {
-  let gender;
-  let hometown;
-
-  let initialDate = data.facebook.birthday.split(/\//);
-  let date = [initialDate[1], initialDate[0], initialDate[2]].join("/");
-
-  if (data.facebook.gender === "male") {
-    gender = "Man";
-  } else if (data.facebook.gender === "female") {
-    gender = "Vrouw";
-  }
-
-  if (data.facebook.hometown.name) {
-    hometown = data.facebook.hometown.name.split(",")[0];
-  } else {
-    hometown = "undefined";
-  }
-
   const result = await client.addJob({
     template: {
       src: `file:///Users/joppe.rabijns/Desktop/NEXRENDER_V1/1080p_Tracking.aep`,
@@ -259,28 +213,21 @@ const end = async (res, data) => {
         type: "data",
         layerName: "NAAM",
         property: "Source Text",
-        value: `${data.facebook.name}`,
-        composition: `END->PANCARTE`,
-      },
-      {
-        type: "data",
-        layerName: "WOONPLAATS",
-        property: "Source Text",
-        value: `${data.facebook.hometown.name.split(",")[0]}`,
+        value: `${data.facebook_name}`,
         composition: `END->PANCARTE`,
       },
       {
         type: "data",
         layerName: "DATUM",
         property: "Source Text",
-        value: `${date}`,
+        value: `${facebook_date}`,
         composition: `END->PANCARTE`,
       },
       {
         type: "data",
         layerName: "GESLACHT",
         property: "Source Text",
-        value: `${gender}`,
+        value: `${data.facebook_gender}`,
         composition: `END->PANCARTE`,
       },
       {
@@ -294,34 +241,34 @@ const end = async (res, data) => {
         type: "data",
         layerName: "EMAIL",
         property: "Source Text",
-        value: `${data.facebook.email}`,
+        value: `${data.facebook_email}`,
         composition: "END->PANCARTE",
       },
       {
         type: "data",
         layerName: "MOMENTEEL",
         property: "Source Text",
-        value: `Omgeving van ${data.fingerprint.ipLocation.city.name}`,
+        value: `Omgeving van ${data.fingerprint_momenteel}`,
         composition: "END->PANCARTE",
       },
       {
         type: "data",
         layerName: "STUDIE",
         property: "Source Text",
-        value: `${data.linkedIn.education}`,
+        value: `${data.linkedin_education}`,
         composition: "END->PANCARTE",
       },
       {
         type: "data",
         layerName: "WERK",
         property: "Source Text",
-        value: `${data.linkedIn.experience}`,
+        value: `${data.linkedin_experience}`,
         composition: "END->PANCARTE",
       },
       {
         type: "image",
         layerName: "FOTO",
-        src: `${data.facebook.picture.data.url}`,
+        src: `${data.facebook_foto}`,
         composition: "END->PANCARTE->FOTO",
       },
     ],
